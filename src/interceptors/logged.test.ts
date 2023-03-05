85426bfc4744c182648154adb6c015bca9d2f881
Ariadna Frias <ariadna.812@gmail.com>
2023-03-05 12:32:50 +0100

    Add logged test file

---
+++ src/interceptors/logged.test.ts
@@ -0,0 +1,39 @@
+import { logged, RequestPlus } from './logged';
+import { Response, NextFunction, Request } from 'express';
+import { Auth } from '../services/auth';
+import { token } from 'morgan';
+
+jest.mock('../services/auth.js');
+
+describe('Given the logged interceptor', () => {
+  const req = {
+    get: jest.fn(),
+    info: {},
+  } as unknown as RequestPlus;
+  const resp = {} as unknown as Response;
+  const next = jest.fn();
+  describe('When we receive a valid token', () => {
+    test('Then the next function will be called', () => {
+      (req.get as jest.Mock).mockReturnValue('test');
+      (Auth.verifyJWTGettingPayload as jest.Mock).mockResolvedValue({
+        payload: 'test',
+      });
+      logged(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we do not receive a valid token', () => {
+    test('Then the catch error will be called', () => {
+      (req.get as jest.Mock).mockReturnValue('');
+      logged(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we do not receive a valid token that starts with "Bearer"', () => {
+    test('Then the catch error will be called', () => {
+      (req.get as jest.Mock).mockReturnValue('');
+      logged(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
