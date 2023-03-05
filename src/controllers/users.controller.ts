2a9f9f43f782965ecd863196a648083d679c0a59
Ariadna Frias <ariadna.812@gmail.com>
2023-03-05 13:39:20 +0100

    Update users controller file

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,6 +5,7 @@ import { User } from '../entities/user.js';
 import createDebug from 'debug';
 import { RequestPlus } from '../interceptors/logged.js';
 import { HTTPError } from '../errors/errors.js';
+import { Auth, PayloadToken } from '../services/auth.js';
 const debug = createDebug('CH7:controller:users');
 
 export class UsersController {
@@ -24,10 +25,16 @@ export class UsersController {
     }
   }
 
-  async get(req: Request, resp: Response, next: NextFunction) {
+  async register(req: Request, resp: Response, next: NextFunction) {
     try {
-      debug('get');
-      const data = await this.repoUsers.queryId(req.params.id);
+      debug('register:post');
+      if (!req.body.email || !req.body.passwd)
+        throw new HTTPError(401, 'Unauthorized', 'Invalid Email or password');
+      req.body.passwd = await Auth.hash(req.body.passwd);
+      req.body.friends = [];
+      req.body.enemies = [];
+      const data = await this.repoUsers.create(req.body);
+      resp.status(201);
       resp.json({
         results: [data],
       });
@@ -36,25 +43,37 @@ export class UsersController {
     }
   }
 
-  async post(req: RequestPlus, resp: Response, next: NextFunction) {
+  async login(req: Request, resp: Response, next: NextFunction) {
     try {
-      debug('post');
-      const userId = req.info?.id;
-      if (!userId) throw new HTTPError(404, 'Not found', 'Not found user id');
-      const actualUser = await this.repoUsers.queryId(userId);
-      req.body.owner = userId;
-      const newThing = await this.repoUsers.create(req.body);
+      debug('login:post');
+      if (!req.body.email || !req.body.passwd)
+        throw new HTTPError(401, 'Unauthorized', 'Invalid Email or password');
+      const data = await this.repoUsers.search({
+        key: 'email',
+        value: req.body.email,
+      });
+      if (!data.length)
+        throw new HTTPError(401, 'Unauthorized', 'Email not found');
+      if (!(await Auth.compare(req.body.passwd, data[0].passwd)))
+        throw new HTTPError(401, 'Unauthorized', 'Password not match');
+      const payload: PayloadToken = {
+        id: data[0].id,
+        email: data[0].email,
+        role: 'admin',
+      };
+      const token = Auth.createJWT(payload);
+      resp.status(202);
       resp.json({
-        results: [newThing],
+        token,
       });
     } catch (error) {
       next(error);
     }
   }
 
-  async patch(req: Request, resp: Response, next: NextFunction) {
+  async addFriend(req: RequestPlus, resp: Response, next: NextFunction) {
     try {
-      debug('patch');
+      debug('adding a friend');
       req.body.id = req.params.id ? req.params.id : req.body.id;
       const data = await this.repoUsers.update(req.body);
       resp.json({
